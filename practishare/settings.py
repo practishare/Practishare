bb7af08561d119d4716ebe764d34396c70577700
Sylvain Le Bon <sylvain@happy-dev.fr>
2014-12-02 23:04:53 +0100

    bugfix: BASE_DIR in settings_specific

--- practishare/settings.py
+++ practishare/settings.py
@@ -7,11 +7,6 @@ https://docs.djangoproject.com/en/1.6/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
 from settings_specific import *
 
 # Quick-start development settings - unsuitable for production
