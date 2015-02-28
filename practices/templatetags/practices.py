58e46a1646cd869fd51861318ec5a2e5dbf277b9
Sylvain Le Bon <sylvain@happy-dev.fr>
2015-02-28 20:07:23 +0100

    feature: send link by email

--- practices/templatetags/practices.py
+++ practices/templatetags/practices.py
@@ -2,5 +2,5 @@ from django import template
 register = template.Library()
 
 @register.filter
-def groupbyaxis2(valuelist, colomnlist):
-    return map(lambda col: filter(lambda p: p.axis2==col, valuelist), colomnlist)
+def groupbyaxis2(valuelist, columnlist):
+    return map(lambda col: filter(lambda p: p.axis2==col, valuelist), columnlist)
