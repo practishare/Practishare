1bc4065bbd874a5fe879946f983179050cbcc450
Sylvain Le Bon <sylvain@happy-dev.fr>
2015-04-03 15:39:21 +0200

    update: python modules in production

--- practishare/wsgi.py
+++ practishare/wsgi.py
@@ -6,10 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
-import os, sys
+import os, sys,site
+
 sys.path.insert(0, '/usr/local/alwaysdata/python/django/1.6.4/')
 sys.path.insert(0, '/home/practishare/practishare')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "practishare.settings")
+site.addsitedir("/home/practishare/.local")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
